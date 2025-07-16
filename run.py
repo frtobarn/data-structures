4b114a918ce0fbcfab359f3fcdaffc01c0ff2577
F4nt0 <frtobarn@unal.edu.co>
2025-07-15 22:59:03 -0500

    could not fix .exe

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app.main import main
 
+import sys, os
+
+def resource_path(relative_path):
+    """Devuelve la ruta absoluta al recurso, ya sea en desarrollo o en exe."""
+    base = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base, relative_path)
+
 if __name__ == "__main__":
     main()
